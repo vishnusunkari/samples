1921a9a4ec941ab78e4b82e6a7d600aee9f2fd6d
vishnu018 <vishnu018.jobs@gmail.com>
2015-08-27 11:30:29 -0700

    Modified

--- JEEDesignPatterns/src/dp/singleton/Database.java
+++ JEEDesignPatterns/src/dp/singleton/Database.java
@@ -32,12 +32,36 @@ public class Database {
 	 *         Create a public static method for getting the reference to the Singleton Object
 	 * Step 3: Make the Access method Synchronized to prevent Thread Problems.
 	 */
-	public static synchronized Database getInstance(String n){
+	/*public static synchronized Database getInstance(String n){
 		if(singleObject == null){
 			singleObject = new Database(n); 
 		}
 		return singleObject;
 	}
+	*/
+	
+	/*
+	 *  Double Checked Locking
+	 *  With double checked locking, we check if instance is created. 
+	 *  If not, then we synchronize creating the object instance.
+	 *  Disadvantage of double checking. 
+	 *  This solution will not work in Java 1.4 or earlier. 
+	 *  Many JVMs in Java version 1.4 and earlier contains implementation 
+	 *  of the volatile keyword that allow improper synchronization of 
+	 *  double checked locking. 
+	 */
+	public static Database getInstance(String n){
+		if(singleObject == null){
+			synchronized(Database.class) {
+				if(singleObject == null) {
+					singleObject = new Database(n);
+				}
+			}
+		}
+		return singleObject;
+	}
+	
+	
 	
 /*	public Database getInstance2(String n){
 		if(singleObject == null){
